522d5b301281f985ba55d9a8c5297289e2e3df34
Rutvik Prjapati <165356998+rutvik3090@users.noreply.github.com>
2024-12-14 12:18:30 +0530

    Update Api.jsx

--- Front-end/Control_Panel/src/Compponents/Api.jsx
+++ Front-end/Control_Panel/src/Compponents/Api.jsx
@@ -1,7 +1,7 @@
 // /src/services/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/users' });
+const API = axios.create({ baseURL: 'https://control-panel-backend.onrender.com' });
 
 export const registerUser = (data) => API.post('/register', data);
 export const loginUser = (data) => API.post('/login', data);
